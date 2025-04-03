8b52ced775a0e61681ab4e0414083c1a11ead742
unknown <emmanuel.kerjean@gmail.com>
2025-04-03 13:40:25 +0200

    TP6

--- src/components/product/product.component.ts
+++ src/components/product/product.component.ts
@@ -1,15 +1,20 @@
 import { Component, input, output } from '@angular/core';
 import { Product } from './product.types';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-product',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css',
 })
 export class ProductComponent {
   product = input.required<Product>();
   addToBasket = output<Product>();
+
+  get limitedStock() {
+    return this.product().stock <= 1;
+  }
 
   onClick() {
     this.addToBasket.emit(this.product());
