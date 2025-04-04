c986803a9232476b7c4877e93c0fda21817a584c
unknown <emmanuel.kerjean@gmail.com>
2025-04-04 09:55:10 +0200

    tp4: ajut getter limitedStock

--- src/components/product/product.component.ts
+++ src/components/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
 import { Product } from './product.types';
 import { NgClass } from '@angular/common';
 
@@ -11,6 +11,7 @@ import { NgClass } from '@angular/common';
 export class ProductComponent {
   product = input.required<Product>();
   addToBasket = output<Product>();
+  mySignal = signal<number>(0);
 
   get limitedStock() {
     return this.product().stock <= 1;
